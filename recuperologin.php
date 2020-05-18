6f4bdda4d322ce9ac7a988fc6ca11f8201925300
Lorenco Malaj <lorenhxz@MacBook-Air-di-Lorenco-3.local>
2020-05-18 23:17:51 +0200

    correzioni grafica

--- recuperologin.php
+++ recuperologin.php
@@ -45,7 +45,21 @@ else
 <head>
     <meta charset="UTF-8">
     <title>Gestione ingressi-uscite</title>
-    <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.1/dist/mini-default.min.css">
+    <!--===============================================================================================-->
+<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="styles.css">
+<!--===============================================================================================-->
 </head>
 
 <body>
